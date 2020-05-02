a2813ef935bd014c81e6c44bbb686c76ad339c08
choianam <58158745+choianam@users.noreply.github.com>
2020-05-02 22:29:35 +0700

    bacabuku

---
+++ bacabuku/app/src/main/java/com/example/bacabuku/_sliders/SliderPagerAdapter.java
@@ -0,0 +1,34 @@
+package com.example.bacabuku._sliders;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class SliderPagerAdapter extends FragmentStatePagerAdapter {
+
+    private static final String TAG = "SliderPagerAdapter";
+
+    List<Fragment> mFrags = new ArrayList<>();
+
+    public SliderPagerAdapter(FragmentManager fm, List<Fragment> frags) {
+        super(fm);
+        mFrags = frags;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        int index = position % mFrags.size();
+        return com.example.bacabuku._sliders.FragmentSlider.newInstance(mFrags.get(index).getArguments().getString("params"));
+    }
+
+    @Override
+    public int getCount() {
+        return Integer.MAX_VALUE;
+    }
+
+}
---
+++ bacabuku/app/src/main/java/com/example/bacabuku/menu.java
@@ -0,0 +1,71 @@
+package com.example.bacabuku;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+    }
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()) {
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.nav_category:
+                            selectedFragment = new CategoryFragment();
+                            break;
+                        case R.id.nav_library:
+                            selectedFragment = new LibraryFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+
+                    //return false;
+                    return true;
+                }
+            };
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.searchmenu, menu);
+        final SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                searchView.clearFocus();
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
+    }
+}
