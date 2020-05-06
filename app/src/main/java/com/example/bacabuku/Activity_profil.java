e26b56e52d4c4b02b522061a799aa184fdff17de
imelia <58154068+imelia@users.noreply.github.com>
2020-05-07 04:54:00 +0700

    Tampilan Profil

---
+++ app/src/main/java/com/example/bacabuku/Activity_profil.java
@@ -0,0 +1,14 @@
+package com.example.bacabuku;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Activity_profil extends AppCompatActivity {
+    private Button action_akun, action_logout;
+    private Intent intent;
+
+}
