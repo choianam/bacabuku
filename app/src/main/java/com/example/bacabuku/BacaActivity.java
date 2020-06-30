27b83b4a179e2db3d123d6bce4be60ff5b396d37
choianam <58158745+choianam@users.noreply.github.com>
2020-06-30 10:59:35 +0700

    up

---
+++ app/src/main/java/com/example/bacabuku/BacaActivity.java
@@ -0,0 +1,21 @@
+package com.example.bacabuku;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.github.barteksc.pdfviewer.PDFView;
+
+public class BacaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_baca);
+
+        PDFView pdfView = (PDFView) findViewById(R.id.pdfView);
+
+        pdfView.fromAsset("Praktikum10.pdf").load();
+    }
+
+}
